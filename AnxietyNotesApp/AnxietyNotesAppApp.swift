f33426f9699fe2bc895135d281eee83c783db3d4
Darma <darmawanruslan12@gmail.com>
2024-07-08 17:19:08 +0700

    Initial Commit

---
+++ AnxietyNotesApp/AnxietyNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnxietyNotesAppApp.swift
+//  AnxietyNotesApp
+//
+//  Created by Dharmawan Ruslan on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AnxietyNotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
