61bd96b3899e16d414d9c4bbe30b6152eb91abab
anggaraswn <anggara11swn@gmail.com>
2024-07-13 17:55:30 +0700

    Add New Note Widget

---
+++ NewNoteWidget/NewNoteWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  NewNoteWidgetBundle.swift
+//  NewNoteWidget
+//
+//  Created by Anggara Satya Wimala Nelwan on 13/07/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct NewNoteWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        NewNoteWidget()
+    }
+}
