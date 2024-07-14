ffe563de65afb3ff756f7f5badc7ccc561b0a554
Agestia Deanara Tori <agestia.dtori@gmail.com>
2024-07-14 13:54:17 +0700

    Added Widget UI and connected to SwiftData

--- AnxietyNotesApp/Views/HomeView.swift
+++ AnxietyNotesApp/Views/HomeView.swift
@@ -8,74 +8,78 @@ struct HomeView: View {
     }
     
     var body: some View {
-        ZStack{
-            Color.bg
-                .ignoresSafeArea()
-            VStack {
-                VStack{
-                    Image(.notebookBro1)
-                        .resizable()
-                        .frame(width: 198, height: 198)
-                    VStack {
-                        Text("Start Writing")
-                            .font(.title3)
-                            .fontWeight(.bold)
-                            .padding(.bottom, 4)
-                        Text("Express and understand your feelings,\n bring clarity to your thoughts.")
-                            .multilineTextAlignment(.center)
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
+        
+        NavigationStack{
+            ZStack{
+                Color.bg
+                    .ignoresSafeArea()
+                VStack {
+                    VStack{
+                        Image(.notebookBro1) // test branch ages
+                            .resizable()
+                            .frame(width: 198, height: 198)
+                        VStack {
+                            Text("Start Writing")
+                                .font(.title3)
+                                .fontWeight(.bold)
+                                .padding(.bottom, 4)
+                            Text("Express and understand your feelings,\n bring clarity to your thoughts.")
+                                .multilineTextAlignment(.center)
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                        }
+                        
                     }
+                    .frame(maxHeight: .infinity, alignment: .center)
+                    .padding(.bottom, 16)
                     
                 }
-                .frame(maxHeight: .infinity, alignment: .center)
-                .padding(.bottom, 16)
-                
-            }
-            .padding()
-            .navigationTitle("NoteInMood")
-            .navigationBarTitleDisplayMode(.large)
-            .toolbar {
-                ToolbarItem(placement: .topBarTrailing)
-                {
-                    Button {
-                        viewModel.showingPopover = true
-                    } label: {
-                        HStack(spacing: 3)
-                        {
-                            Image(systemName: "line.3.horizontal.decrease.circle.fill")
+                .padding()
+                .navigationTitle("NoteInMood")
+                .navigationBarTitleDisplayMode(.large)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing)
+                    {
+                        Button {
+                            viewModel.showingPopover = true
+                        } label: {
+                            HStack(spacing: 3)
+                            {
+                                Image(systemName: "line.3.horizontal.decrease.circle.fill")
+                                    .foregroundColor(.indigo)
+                            }
+                        }
+                        .popover(isPresented: $viewModel.showingPopover) {
+                            Text("Your content here")
+                                .font(.headline)
+                                .padding()
+                                .frame(minWidth: 200, minHeight: 200)
+                                .presentationCompactAdaptation(.popover)
+                        }
+                        
+                        //                        NavigationLink(destination: self) {
+                        //                                Image(systemName: "line.3.horizontal.decrease.circle.fill")
+                        //                                    .foregroundColor(.indigo)
+                        //                        }
+                    }
+                    ToolbarItem(placement: .bottomBar)
+                    {
+                        NavigationLink(destination: ContentView() .navigationBarBackButtonHidden(true)) {
+                            
+                            Image(systemName: "square.and.pencil")
                                 .foregroundColor(.indigo)
                         }
                     }
-                    .popover(isPresented: $viewModel.showingPopover) {
-                        Text("Your content here")
-                            .font(.headline)
-                            .padding()
-                            .frame(minWidth: 200, minHeight: 200)
-                            .presentationCompactAdaptation(.popover)
-                    }
-                    
-                    //                        NavigationLink(destination: self) {
-                    //                                Image(systemName: "line.3.horizontal.decrease.circle.fill")
-                    //                                    .foregroundColor(.indigo)
-                    //                        }
                 }
-                ToolbarItem(placement: .bottomBar)
-                {
-                    NavigationLink(destination: ContentView() .navigationBarBackButtonHidden(true)) {
-                        
-                        Image(systemName: "square.and.pencil")
-                            .foregroundColor(.indigo)
-                    }
+                .toolbarBackground(.visible, for: .bottomBar)
+            }
+            .onChange(of: viewModel.notes) {
+                for note in viewModel.notes{
+                    print(note.title ?? "")
                 }
             }
-            .toolbarBackground(.visible, for: .bottomBar)
         }
-        .onChange(of: viewModel.notes) {
-            for note in viewModel.notes{
-                print(note.title ?? "")
-            }
-        }
+
     }
     
     
